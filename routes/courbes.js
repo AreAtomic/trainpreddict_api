b69cf2f17f8dc464b1043357f301821719130d21
AreAtomic <aureliensebe@gmail.com>
2021-07-18 10:22:58 +0200

    Génération des courbes si elles ne le sont pas

--- routes/courbes.js
+++ routes/courbes.js
@@ -21,12 +21,131 @@ const Plan = require('../models/Plan')
  * @description Récupérations des courbes
  */
 router.get('/', [jwtauth], async (req, res) => {
-    const courbesPrev = await CourbePrev.findOne({
+    let courbesPrev = await CourbePrev.findOne({
         _utilisateur: req.utilisateur._id,
     })
-    const courbesRea = await CourbeRea.findOne({
+    let courbesRea = await CourbeRea.findOne({
         _utilisateur: req.utilisateur._id,
     })
+
+    // Gen si nul
+    if (!courbesRea) {
+        const utilisateur = req.utilisateur._id
+        const plans = await Plan.find({ _utilisateur: utilisateur })
+        let forme = []
+        let fatigue = []
+        let tss = []
+        let labels = []
+
+        // Création des labels et des courbes de 2000 à 2100
+        for (let i = 2000; i < 2101; i++) {
+            let days = []
+            let array_year = []
+            let lastday = dayjs(`${i}-12-31`).dayOfYear()
+
+            for (let j = 1; j < lastday; j++) {
+                days.push(dayjs(`${i}-01-01`).dayOfYear(j).format('MM/DD/YYYY'))
+                array_year.push(0)
+            }
+            forme.push(array_year)
+            fatigue.push(array_year)
+            labels.push(days)
+        }
+
+        // Rajout des données des plans dans les courbes
+        for (let i = 0; i < plans.length; i++) {
+            let seances_definies = plans[i].SeancesDefinies
+
+            for (let j = 0; j < seances_definies.length; j++) {
+                const seance = seances_definies[j]
+                let year = dayjs(seance[1]).year() - 2000
+                let day = dayjs(seance[1]).dayOfYear()
+
+                if (typeof seance[0] != 'string') {
+                    tss.push(seance[0].score_stress_entrainement)
+                } else if (seance[0] == 'repos') {
+                    tss.push(0)
+                } else if (seance[0] == 'musculation') {
+                    tss.push(50)
+                } else if (seance[0] == 'course à pied') {
+                    tss.push(75)
+                }
+
+                let tss_forme = []
+                let tss_fatigue = []
+                if (tss.length < 42) {
+                    tss_forme = new Array(42 - tss.length).fill(0)
+                    if (tss.length < 7) {
+                        tss_fatigue = new Array(7 - tss.length).fill(0)
+                    }
+                }
+                tss_forme = tss.concat(tss_forme)
+                forme[year][day] = moyenneArray(
+                    tss_forme.slice(tss_forme.length - 42, tss_forme.length)
+                )
+                tss_fatigue = tss.concat(tss_fatigue)
+                fatigue[year][day] = moyenneArray(
+                    tss_fatigue.slice(
+                        tss_fatigue.length - 7,
+                        tss_fatigue.length
+                    )
+                )
+            }
+        }
+
+        // Update de la bdd
+        courbesPrev = await CourbePrev.findOneAndUpdate(
+            { _utilisateur: utilisateur },
+            { $set: { forme: forme, fatigue: fatigue, labels: labels } },
+            { new: true, upsert: true }
+        )
+
+        if (!courbesPrev) {
+            courbesPrev = new CourbePrev({
+                _utilisateur: utilisateur,
+                forme: forme,
+                fatigue: fatigue,
+                labels: labels,
+            })
+        }
+
+        courbesPrev.save()
+    }
+
+    if (!courbesRea) {
+        const utilisateur = req.utilisateur._id
+        let forme = []
+        let fatigue = []
+        let labels = []
+
+        // Création des labels et des courbes de 2000 à 2100
+        for (let i = 2000; i < 2101; i++) {
+            let days = []
+            let array_year = []
+            let lastday = dayjs(`${i}-12-31`).dayOfYear()
+
+            for (let j = 1; j < lastday; j++) {
+                days.push(dayjs(`${i}-01-01`).dayOfYear(j).format('MM/DD/YYYY'))
+                array_year.push(0)
+            }
+            forme.push(array_year)
+            fatigue.push(array_year)
+            labels.push(days)
+        }
+
+        courbesRea = await CourbeRea.findOne({ _utilisateur: utilisateur })
+
+        if (!courbesRea) {
+            courbesRea = new CourbeRea({
+                _utilisateur: utilisateur,
+                forme: forme,
+                fatigue: fatigue,
+                labels: labels,
+            })
+        }
+
+        courbesRea.save()
+    }
 
     return res
         .status(200)
