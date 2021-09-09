4c028d724ec73e8b3f644402f6b591d328992b80
AreAtomic <aureliensebe@gmail.com>
2021-09-09 06:50:15 +0200

    Modification bug statistiques

--- routes/statistiques.js
+++ routes/statistiques.js
@@ -50,9 +50,126 @@ const sort = (tab) => {
  */
 router.get('/', [jwtauth], async (req, res) => {
     try {
-        const statistiques = await Statistiques.findOne({
+        let statistiques = await Statistiques.findOne({
             _utilisateur: req.utilisateur._id,
         })
+        if (!statistiques) {
+            const month = {
+                janvier: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                fevrier: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                mars: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                avril: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                mai: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                juin: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                juillet: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                aout: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                septembre: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                octobre: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                novembre: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+                decembre: {
+                    kilometres: 0,
+                    heures: 0,
+                    sse: 0,
+                    nombre_entrainement: 0,
+                    gain_forme: +0,
+                },
+            }
+
+            const entrainement = []
+            for (let i = 2000; i < 2100; i++) {
+                let year = { [i]: { mois: month, semaines: {} } }
+                let weeks = {}
+                const weekofyear = dayjs(`${i}-01-01`).isoWeeksInYear()
+                for (let j = 1; j <= weekofyear; j++) {
+                    weeks[`S${j}`] = {
+                        kilometres: 0,
+                        heures: 0,
+                        sse: 0,
+                        nombre_entrainement: 0,
+                        gain_forme: +0,
+                    }
+                }
+                year[i].semaines = weeks
+                entrainement.push(year)
+            }
+
+            statistiques = new Statistiques({
+                _utilisateur: req.utilisateur.id,
+                entrainement: entrainement,
+                reccord_20_minutes: 0,
+                reccord_5_minutes: 0,
+                reccord_1_minutes: 0,
+                recourd_5_seconds: 0,
+            })
+            statistiques.save()
+        }
+        console.log(statistiques)
         return res.status(200).json({
             data: statistiques,
             msg: 'Statstiques récupérées avec succès',
