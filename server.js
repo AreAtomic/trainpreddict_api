63355c7eeb7efc59ebd4242994972095e6b0eb3b
AreAtomic <aureliensebe@gmail.com>
2021-10-12 14:13:01 +0200

    Modif preprod

--- server.js
+++ server.js
@@ -78,12 +78,6 @@ if (process.env.NODE_ENV != 'development') {
         ca: ca,
     }
 
-    const httpServer = http.createServer((req, res) => {
-        res.writeHead(301, {
-            Location: `https://${req.headers.host}${req.url}`,
-        })
-        res.end()
-    })
     const httpsServer = https.createServer(credentials, app).listen(6001)
 }
 
@@ -177,6 +171,6 @@ app.use(
 )
 
 if (process.env.NODE_ENV == 'development') {
-    const PORT = process.env.PORT || 5000
+    const PORT = process.env.PORT || 6001
     app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
 }
